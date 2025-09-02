abc06ecd7dfb04c36c52b0a8cc226a2ea248508e
rafaw <rafa.willian91@gmail.com>
2025-09-02 04:33:22 -0300

    propriedades customizadas com @Value

--- 02.23-criando-eacessando-propriedades-customizadas-comValue/src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ 02.23-criando-eacessando-propriedades-customizadas-comValue/src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.di.notificacao;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.modelo.Cliente;
@@ -7,9 +8,17 @@ import com.algaworks.algafood.di.modelo.Cliente;
 @TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
 @Component
 public class NotificadorEmail implements Notificador {
-	
+
+	@Value("${notificador.email.host-url}")
+	private String host;
+
+	@Value("${notificador.email.port-port}")
+	private Integer port;
+
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
+		System.out.println("Host: " + host);
+		System.out.println("Porta: " + port);
 		System.out.printf("Notificando %s através do e-mail %s: %s\n", 
 				cliente.getNome(), cliente.getEmail(), mensagem);
 	}
