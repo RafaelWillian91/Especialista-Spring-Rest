d5b46eebae942ebb7909346d72fdbfa3485f9279
rafaw <rafa.willian91@gmail.com>
2025-09-09 01:53:25 -0300

    Corrige .gitignore e remove artefatos do repositório

--- 3.00-algafood-API-JPA/target/classes/import.sql
+++
@@ -1,2 +0,0 @@
-insert into tab_cozinha (nome) values ('Tailandesa');
-insert into tab_cozinha (nome) values ('Indiana');
--- 3.01-Consultando objetos do banco de dados/target/classes/import.sql
+++
@@ -1,5 +0,0 @@
---  Inserindo restaurantes de teste
---  Esses dados são usados apenas em desenvolvimento/teste
-
-insert into tab_cozinha (nome) values ('Tailandesa');
-insert into tab_cozinha (nome) values ('Indiana');
