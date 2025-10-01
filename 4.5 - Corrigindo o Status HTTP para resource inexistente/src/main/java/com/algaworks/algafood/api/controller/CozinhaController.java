0d5a359ec2860c543b9db099a2cab82724ce8c95
rafaw <rafa.willian91@gmail.com>
2025-10-01 01:34:16 -0300

    Corrigindo o Status HTTP para resource inexistente

---
+++ 4.5 - Corrigindo o Status HTTP para resource inexistente/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -0,0 +1,50 @@
+package com.algaworks.algafood.api.controller;
+
+import java.util.List;
+
+import com.algaworks.algafood.api.model.CozinhasXMLWrapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+
+
+	@GetMapping
+	public List<Cozinha> listar() {
+
+		return cozinhaRepository.listar();
+	}
+
+	//retorno embrulho de cozinha
+	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+	public CozinhasXMLWrapper listarXml(){
+		return new CozinhasXMLWrapper(cozinhaRepository.listar());
+	}
+
+	@GetMapping("/{cozinhaId}")
+	public ResponseEntity<Cozinha> buscaCozinha(@PathVariable Long cozinhaId) {
+
+		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+		if(cozinha != null){
+			return ResponseEntity.ok(cozinha);
+		}
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		 
+	}
+	
+}
---
+++ 4.5 - Corrigindo o Status HTTP para resource inexistente/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -0,0 +1,31 @@
+package com.algaworks.algafood.domain.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+//@JsonRootName("gastronomia") // muda o nome da propriedade do elemento. No caso de xml
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Cozinha {
+
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	//@JsonIgnore//Remove a propriedade da representacao de saida do controle para Json ou xml
+	@JsonProperty(value = "titulo")//muda a representacao da propriedade de "nome" para "titulo"
+	@Column(nullable = false)
+	private String nome;
+
+}
