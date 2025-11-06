49417ee72f3a26317fdf2e991ac03237385646ff
rafaw <rafa.willian91@gmail.com>
2025-11-06 12:56:03 -0300

    Mapeando relacionamento muitos-para-muitos com @ManyToMany

---
+++ 6.0 - Mapeando relacionamento muitos-para-muitos com @ManyToMany/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -0,0 +1,38 @@
+package com.algaworks.algafood.domain.model;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.*;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Restaurante {
+
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String nome;
+	
+	@Column(name = "taxa_frete", nullable = false)
+	private BigDecimal taxaFrete;
+	
+	@ManyToOne
+	@JoinColumn(name = "cozinha_id", nullable = false)
+	private Cozinha cozinha;
+
+	@ManyToMany
+	@JoinTable(name = "restaurante_forma_pagamento", //A entidade que declara o @JoinTable é a dona da relação
+	joinColumns = @JoinColumn(name = "restaurante_id"),
+	inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
+	private List<FormaPagamento> formasPagamentos = new ArrayList<>();
+	
+}
