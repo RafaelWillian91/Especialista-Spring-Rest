acd2aadf0680fd429a6b79b33721851be6f54120
rafaw <rafa.willian91@gmail.com>
2025-11-12 20:07:22 -0300

    Mapeando propriedades com @CreationTimestamp e @UpdateTimestamp

---
+++ 6.2 - Mapeando propriedades com @CreationTimestamp e @UpdateTimestamp/src/main/java/com/algaworks/algafood/domain/model/Endereco.java
@@ -0,0 +1,29 @@
+package com.algaworks.algafood.domain.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Embeddable // classe incorporavel. ele é uma parte de outra entidade e ele vai fazer parte da tabela da entidade
+public class Endereco {
+
+    @Column(name = "endereco_cep")
+    private String cep;
+
+    @Column(name = "endereco_logradouro")
+    private String logradouro;
+
+    @Column(name = "endereco_numero")
+    private String numero;
+    @Column(name = "endereco_complemento")
+    private String complemento;
+    @Column(name = "endereco_bairro")
+    private String bairro;
+
+    @ManyToOne
+    @JoinColumn(name = "endereco_cidade_id")
+    private Cidade cidade;
+
+
+}
