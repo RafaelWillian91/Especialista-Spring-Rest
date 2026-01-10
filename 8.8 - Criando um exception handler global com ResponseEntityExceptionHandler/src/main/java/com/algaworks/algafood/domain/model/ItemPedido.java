f09f4f7a88e91f9e33f14ebcd7eb138a68e37424
rafaw <rafa.willian91@gmail.com>
2026-01-09 21:39:33 -0300

    Criando um exception handler global com ResponseEntityExceptionHandler

---
+++ 8.8 - Criando um exception handler global com ResponseEntityExceptionHandler/src/main/java/com/algaworks/algafood/domain/model/ItemPedido.java
@@ -0,0 +1,38 @@
+package com.algaworks.algafood.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.List;
+
+@Entity
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class ItemPedido {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer quantidade;
+
+    private BigDecimal precoUnitario;
+
+    private BigDecimal precoTotal;
+
+    private String observacao;
+
+    @ManyToOne
+    @JoinColumn(name = "pedido_id", nullable = false)
+    private Pedido pedido;
+
+    @ManyToOne
+    @JoinColumn(name = "produto_id", nullable = false)
+    private Produto produto;
+
+
+
+}
