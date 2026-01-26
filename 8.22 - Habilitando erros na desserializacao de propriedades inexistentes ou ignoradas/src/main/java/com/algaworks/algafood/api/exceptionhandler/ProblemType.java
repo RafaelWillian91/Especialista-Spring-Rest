0326cde35423a4f47a30ba0856b905acc5d0e320
rafaw <rafa.willian91@gmail.com>
2026-01-26 00:57:35 -0300

    8.22 - Habilitando erros na desserializacao de propriedades inexistentes ou ignoradas. Erros de parametro de url

---
+++ 8.22 - Habilitando erros na desserializacao de propriedades inexistentes ou ignoradas/src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
@@ -0,0 +1,30 @@
+package com.algaworks.algafood.api.exceptionhandler;
+
+import lombok.Getter;
+
+
+public enum ProblemType {
+
+        MENSAGEM_INCOMPREENSIVEL("mensagem-imcompreensivel", "Mensagem imcompreensível"),
+        ENTIDADE_NAO_ENCONTRADA("entidade-nao-encontrada", "Entidade não encontrada"),
+        ENTIDADE_EM_USO("entida-em-uso", "Entidade em uso"),
+        ERRO_NEGOCIO("erro-de-Negócio", "Violação de regra de Negócio"),
+
+        PARAMETRO_INVALIDO("erro-de-parametro", "Corrija o parâmetro informado");
+
+        private String title;
+        private String uri;
+
+        private ProblemType (String path, String title){
+            this.uri = "https://algafood.com/" + path;
+            this.title = title;
+        }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
