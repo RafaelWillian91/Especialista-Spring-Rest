797e2f3e508e5da868274a502923e5aa2f678977
rafaw <rafa.willian91@gmail.com>
2026-01-31 05:15:46 -0300

    Validando as associações de uma entidade

---
+++ 9 - Beans Validations/9.2 - Adicionando constraints e validando no controller com Valid/src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
@@ -0,0 +1,31 @@
+package com.algaworks.algafood.api.exceptionhandler;
+
+import lombok.Getter;
+
+
+public enum ProblemType {
+
+        MENSAGEM_INCOMPREENSIVEL("mensagem-imcompreensivel", "Mensagem imcompreensível"),
+        RECURSO_NAO_ENCONTRADO("recurso-nao-encontrado", "Recurso não encontrado"),
+        ENTIDADE_EM_USO("entida-em-uso", "Entidade em uso"),
+        ERRO_NEGOCIO("erro-de-Negócio", "Violação de regra de Negócio"),
+        PARAMETRO_INVALIDO("erro-de-parametro", "Corrija o parâmetro informado"),
+        ERRO_DE_SISTEMA("erro-de-sistema", "ERRO_DE_SISTEMA"),
+
+        DADOS_INVALIDOS("dados-inválidos", "Dados inválidos");
+        private String title;
+        private String uri;
+
+        private ProblemType (String path, String title){
+            this.uri = "https://algafood.com/" + path;
+            this.title = title;
+        }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
---
+++ 9 - Beans Validations/9.2 - Adicionando constraints e validando no controller com Valid/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -0,0 +1,38 @@
+package com.algaworks.algafood.domain.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonRootName;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@JsonRootName("cozinha")
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Cozinha {
+
+	@NotNull
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank
+	@Column(nullable = false)
+	private String nome;
+
+
+
+	@JsonIgnore
+	@OneToMany(mappedBy = "cozinha")
+	private List<Restaurante> restaurantes = new ArrayList<>();
+	
+}
