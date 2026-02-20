cc2f073141e761739eadaf670c47d50864cc1609
rafaw <rafa.willian91@gmail.com>
2026-02-19 21:16:16 -0300

    02 - escrevendo teste de integracao com JUnit

---
+++ 10 - teste de integracao/02 - Escrevendo bons nomes de testes/algafood-api/src/test/java/com/algaworks/algafood/CadastroCozinhaIntegrationTests.java
@@ -0,0 +1,81 @@
+package com.algaworks.algafood;
+
+import com.algaworks.algafood.domain.exception.NegocioException;
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import com.algaworks.algafood.domain.service.CadastroCozinhaService;
+import com.algaworks.algafood.domain.service.CadastroRestauranteService;
+import org.assertj.core.api.Assert;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintViolationException;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("dev")
+public class CadastroCozinhaIntegrationTests {
+
+	@Autowired
+	CadastroCozinhaService cozinhaService;
+
+	@Autowired
+	CadastroRestauranteService cadastroRestauranteService;
+	@Test
+	public void deveAtribuirId_QuandoCadastrarCozinhaComDadosCorretos(){
+		//cenário
+		Cozinha cozinha = new Cozinha();
+		cozinha.setNome("restaurante-Teste");
+		//acao
+		cozinhaService.salvar(cozinha);
+		//validacao
+		Assertions.assertThat(cozinha).isNotNull();
+		Assertions.assertThat(cozinha.getId()).isNotNull();
+	}
+
+	@Test
+	public void deveFalhar_QuandoCadastrarCozinhaSemNome() {
+		Cozinha novaCozinha = new Cozinha();
+		novaCozinha.setNome(null);
+
+		ConstraintViolationException erroEsperado =
+				assertThrows(ConstraintViolationException.class, () -> {
+					cozinhaService.salvar(novaCozinha);
+				});
+
+	}
+
+	@Test
+	public void deveFalhar_QuandoExcluirCozinhaEmUso(){
+
+		Cozinha cozinhaUso = new Cozinha();
+		cozinhaUso.setId(19L);
+		Restaurante restaurante = new Restaurante();
+		restaurante.setCozinha(cozinhaUso);
+
+		NegocioException erroEsperado =
+				assertThrows(NegocioException.class, () -> {
+					cozinhaService.excluir(19L);
+				});
+	}
+
+	@Test
+	public void deveFalhar_QuandoExcluirCozinhaInexistente(){
+
+		NegocioException erroEsperado =
+				assertThrows(NegocioException.class, () -> {
+					cozinhaService.excluir(60L);
+				});
+	}
+}
