6c23eafdf41d0d2718f317e7ea6311e552e791c1
rafaw <rafa.willian91@gmail.com>
2026-03-17 22:48:50 -0300

    Isolando o Domain Model do Representation Model com o padrão DTO

---
+++ 11 - Boas praticas e tecnicas para APIs/05 - Isolando o Domain Model do Representation Model com o padrao DTO/algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -0,0 +1,137 @@
+package com.algaworks.algafood.api.controller;
+
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import javax.validation.ValidationException;
+
+import com.algaworks.algafood.api.model.CozinhaDTO;
+import com.algaworks.algafood.api.model.RestauranteDTO;
+import com.algaworks.algafood.api.model.input.RestauranteInput;
+import com.algaworks.algafood.core.validation.ValidacaoException;
+import com.algaworks.algafood.domain.model.Cozinha;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.SmartValidator;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.algafood.domain.exception.CozinhaNaoEncontradaException;
+import com.algaworks.algafood.domain.exception.NegocioException;
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.repository.RestauranteRepository;
+import com.algaworks.algafood.domain.service.CadastroRestauranteService;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@RestController
+@RequestMapping(value = "/restaurantes")
+public class RestauranteController {
+
+	@Autowired
+	private RestauranteRepository restauranteRepository;
+	
+	@Autowired
+	private CadastroRestauranteService cadastroRestaurante;
+
+	//api de validacao manual
+	@Autowired
+	private SmartValidator smartValidator;
+	
+	@GetMapping
+	public List<RestauranteDTO> listar() {
+
+		return toCollectionModel(restauranteRepository.findAll());
+	}
+	
+	@GetMapping("/{restauranteId}")
+	public RestauranteDTO buscar(@PathVariable Long restauranteId) {
+		Restaurante restaurante =  cadastroRestaurante.buscarOuFalhar(restauranteId);
+
+		return toModel(restaurante);
+	}
+
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public RestauranteDTO adicionar(@RequestBody @Valid RestauranteInput restauranteInput) {
+
+		try {
+			Restaurante restaurante = toDomainObject(restauranteInput);
+
+			return toModel(cadastroRestaurante.salvar(restaurante));
+		} catch (CozinhaNaoEncontradaException e) {
+			throw new NegocioException(e.getMessage());
+		}
+	}
+	
+	@PutMapping("/{restauranteId}")
+	public RestauranteDTO atualizar(@PathVariable Long restauranteId,
+			@RequestBody @Valid RestauranteInput restauranteInput) {
+		try {
+			Restaurante restaurante = toDomainObject(restauranteInput);
+			Restaurante restauranteAtual = cadastroRestaurante.buscarOuFalhar(restauranteId);
+
+			BeanUtils.copyProperties(restaurante, restauranteAtual,
+					"id", "formasPagamento", "endereco", "dataCadastro", "produtos");
+
+			return toModel(cadastroRestaurante.salvar(restauranteAtual));
+		} catch (CozinhaNaoEncontradaException e) {
+			throw new NegocioException(e.getMessage());
+		}
+	}
+
+	private static RestauranteDTO toModel(Restaurante restaurante) {
+		CozinhaDTO cozinhaDTO = new CozinhaDTO();
+		cozinhaDTO.setId(restaurante.getCozinha().getId());
+		cozinhaDTO.setNome(restaurante.getCozinha().getNome());
+
+		RestauranteDTO restauranteDTO = new RestauranteDTO();
+		restauranteDTO.setId(restaurante.getId());
+		restauranteDTO.setNome(restaurante.getNome());
+		restauranteDTO.setTaxaFrete(restaurante.getTaxaFrete());
+		restauranteDTO.setCozinha(cozinhaDTO);
+		return restauranteDTO;
+	}
+
+	private List<RestauranteDTO> toCollectionModel (List<Restaurante> restaurantes){
+
+		return restaurantes.stream()
+				.map(restaurante -> toModel(restaurante))
+				.collect(Collectors.toList());
+
+	}
+
+	private Restaurante toDomainObject(RestauranteInput restauranteInput){
+
+		Restaurante restaurante = new Restaurante();
+		restaurante.setNome(restauranteInput.getNome());
+		restaurante.setTaxaFrete(restauranteInput.getTaxaFrete());
+
+		Cozinha cozinha = new Cozinha();
+		cozinha.setId(restauranteInput.getCozinha().getId());
+
+		restaurante.setCozinha(cozinha);
+
+		return restaurante;
+	}
+	
+}
---
+++ 11 - Boas praticas e tecnicas para APIs/05 - Isolando o Domain Model do Representation Model com o padrao DTO/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -0,0 +1,68 @@
+package com.algaworks.algafood.domain.model;
+
+import com.algaworks.algafood.core.validation.ValorZeroIncluiDescricao;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@ValorZeroIncluiDescricao(valorField = "taxaFrete", descricaoField = "nome", descricaoObrigatoria = "Frete Grátis")
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Restaurante {
+
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	//@NotBlank
+	@Column(nullable = false)
+	private String nome;
+	
+	//@NotNull
+	//@PositiveOrZero
+	@Column(name = "taxa_frete", nullable = false)
+	private BigDecimal taxaFrete;
+
+
+	//@Valid
+	//@ConvertGroup(from = Default.class, to = Groups.CozinhaId.class)
+	//@NotNull
+	@ManyToOne
+	@JoinColumn(name = "cozinha_id", nullable = false)
+	private Cozinha cozinha;
+	
+
+	@Embedded
+	private Endereco endereco;
+	
+
+	@CreationTimestamp
+	@Column(nullable = false, columnDefinition = "datetime")
+	private OffsetDateTime dataCadastro;
+	
+
+	@UpdateTimestamp
+	@Column(nullable = false, columnDefinition = "datetime")
+	private OffsetDateTime dataAtualizacao;
+	
+
+	@ManyToMany
+	@JoinTable(name = "restaurante_forma_pagamento",
+			joinColumns = @JoinColumn(name = "restaurante_id"),
+			inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
+	private List<FormaPagamento> formasPagamento = new ArrayList<>();
+	
+
+	@OneToMany(mappedBy = "restaurante")
+	private List<Produto> produtos = new ArrayList<>();
+	
+}
