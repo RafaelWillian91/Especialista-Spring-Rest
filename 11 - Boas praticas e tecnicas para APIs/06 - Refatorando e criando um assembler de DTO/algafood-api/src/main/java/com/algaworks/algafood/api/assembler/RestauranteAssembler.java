8bd370aa9d880c7a3f9c0a50b6dec3292c3e6d4c
rafaw <rafa.willian91@gmail.com>
2026-03-18 04:48:01 -0300

    Refatorando e criando um assembler de DTO

---
+++ 11 - Boas praticas e tecnicas para APIs/06 - Refatorando e criando um assembler de DTO/algafood-api/src/main/java/com/algaworks/algafood/api/assembler/RestauranteAssembler.java
@@ -0,0 +1,36 @@
+package com.algaworks.algafood.api.assembler;
+
+import com.algaworks.algafood.api.model.CozinhaDTO;
+import com.algaworks.algafood.api.model.RestauranteDTO;
+import com.algaworks.algafood.domain.model.Restaurante;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class RestauranteAssembler {
+
+    public static RestauranteDTO toModel(Restaurante restaurante) {
+        CozinhaDTO cozinhaDTO = new CozinhaDTO();
+        cozinhaDTO.setId(restaurante.getCozinha().getId());
+        cozinhaDTO.setNome(restaurante.getCozinha().getNome());
+
+        RestauranteDTO restauranteDTO = new RestauranteDTO();
+        restauranteDTO.setId(restaurante.getId());
+        restauranteDTO.setNome(restaurante.getNome());
+        restauranteDTO.setTaxaFrete(restaurante.getTaxaFrete());
+        restauranteDTO.setCozinha(cozinhaDTO);
+        return restauranteDTO;
+    }
+
+    public List<RestauranteDTO> toCollectionModel(List<Restaurante> restaurantes) {
+
+        return restaurantes.stream()
+                .map(restaurante -> toModel(restaurante))
+                .collect(Collectors.toList());
+
+    }
+
+
+}
---
+++ 11 - Boas praticas e tecnicas para APIs/06 - Refatorando e criando um assembler de DTO/algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -0,0 +1,80 @@
+package com.algaworks.algafood.api.controller;
+
+import com.algaworks.algafood.api.assembler.RestauranteAssembler;
+import com.algaworks.algafood.api.assembler.RestauranteDisassembler;
+import com.algaworks.algafood.api.model.RestauranteDTO;
+import com.algaworks.algafood.api.model.input.RestauranteInput;
+import com.algaworks.algafood.domain.exception.CozinhaNaoEncontradaException;
+import com.algaworks.algafood.domain.exception.NegocioException;
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.repository.RestauranteRepository;
+import com.algaworks.algafood.domain.service.CadastroRestauranteService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.SmartValidator;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/restaurantes")
+public class RestauranteController {
+
+    @Autowired
+    private RestauranteRepository restauranteRepository;
+
+    @Autowired
+    private CadastroRestauranteService cadastroRestaurante;
+
+    @Autowired
+    private SmartValidator smartValidator;
+
+    @Autowired
+    private RestauranteAssembler restauranteAssembler;
+    @Autowired
+    private RestauranteDisassembler restauranteDisassembler;
+    @GetMapping
+    public List<RestauranteDTO> listar() {
+
+        return restauranteAssembler.toCollectionModel(restauranteRepository.findAll());
+    }
+
+    @GetMapping("/{restauranteId}")
+    public RestauranteDTO buscar(@PathVariable Long restauranteId) {
+        Restaurante restaurante = cadastroRestaurante.buscarOuFalhar(restauranteId);
+
+        return restauranteAssembler.toModel(restaurante);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public RestauranteDTO adicionar(@RequestBody @Valid RestauranteInput restauranteInput) {
+
+        try {
+            Restaurante restaurante = restauranteDisassembler.toDomainObject(restauranteInput);
+
+            return restauranteAssembler.toModel(cadastroRestaurante.salvar(restaurante));
+        } catch (CozinhaNaoEncontradaException e) {
+            throw new NegocioException(e.getMessage());
+        }
+    }
+
+    @PutMapping("/{restauranteId}")
+    public RestauranteDTO atualizar(@PathVariable Long restauranteId,
+                                    @RequestBody @Valid RestauranteInput restauranteInput) {
+        try {
+            Restaurante restaurante = restauranteDisassembler.toDomainObject(restauranteInput);
+            Restaurante restauranteAtual = cadastroRestaurante.buscarOuFalhar(restauranteId);
+
+            BeanUtils.copyProperties(restaurante, restauranteAtual,
+                    "id", "formasPagamento", "endereco", "dataCadastro", "produtos");
+
+            return restauranteAssembler.toModel(cadastroRestaurante.salvar(restauranteAtual));
+        } catch (CozinhaNaoEncontradaException e) {
+            throw new NegocioException(e.getMessage());
+        }
+    }
+
+}
