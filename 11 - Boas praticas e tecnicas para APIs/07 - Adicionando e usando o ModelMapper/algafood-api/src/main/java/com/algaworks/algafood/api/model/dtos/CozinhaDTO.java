dff1bc1501bc3a26a2262838e9acbea950cd4400
rafaw <rafa.willian91@gmail.com>
2026-03-22 12:12:09 -0300

    Adicionando e usando ModelMapper

---
+++ 11 - Boas praticas e tecnicas para APIs/07 - Adicionando e usando o ModelMapper/algafood-api/src/main/java/com/algaworks/algafood/api/model/dtos/CozinhaDTO.java
@@ -0,0 +1,13 @@
+package com.algaworks.algafood.api.model.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class CozinhaDTO {
+
+    private Long id;
+
+    private String nome;
+}
