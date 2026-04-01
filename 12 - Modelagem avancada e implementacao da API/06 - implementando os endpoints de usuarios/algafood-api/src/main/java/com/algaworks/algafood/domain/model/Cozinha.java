eb74372699ba53867fb54430fcc338f5bbba0230
rafaw <rafa.willian91@gmail.com>
2026-04-01 20:10:28 -0300

    implementando os endpoints de usuarios

---
+++ 12 - Modelagem avancada e implementacao da API/06 - implementando os endpoints de usuarios/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -0,0 +1,29 @@
+package com.algaworks.algafood.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Cozinha {
+
+	//@NotNull(groups = Groups.CozinhaId.class)
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	//@NotBlank
+	@Column(nullable = false)
+	private String nome;
+	
+
+	@OneToMany(mappedBy = "cozinha")
+	private List<Restaurante> restaurantes = new ArrayList<>();
+	
+}
