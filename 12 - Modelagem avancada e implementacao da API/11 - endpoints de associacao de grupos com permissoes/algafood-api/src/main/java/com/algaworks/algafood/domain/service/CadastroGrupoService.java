fa2c7b72901e4be591dc4074c2ac90072f736880
rafaw <rafa.willian91@gmail.com>
2026-04-09 05:37:08 -0300

    implementando os endpoints de associação de grupos com permissões

---
+++ 12 - Modelagem avancada e implementacao da API/11 - endpoints de associacao de grupos com permissoes/algafood-api/src/main/java/com/algaworks/algafood/domain/service/CadastroGrupoService.java
@@ -0,0 +1,67 @@
+package com.algaworks.algafood.domain.service;
+
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
+import com.algaworks.algafood.domain.exception.GrupoNaoEncontradoException;
+import com.algaworks.algafood.domain.model.Grupo;
+import com.algaworks.algafood.domain.model.Permissao;
+import com.algaworks.algafood.domain.repository.GrupoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class CadastroGrupoService {
+
+    private final String MSG_GRUPO_EM_USO = "Grupo de código %d não pode ser removido, pois está em uso";
+
+    @Autowired
+    private GrupoRepository grupoRepository;
+    @Autowired
+    private CadastroPermissaoService cadastroPermissaoService;
+
+    @Transactional
+    public Grupo salvar(Grupo grupo){
+        return grupoRepository.save(grupo);
+    }
+
+    @Transactional
+    public void excluirGrupo(Long grupoId){
+        try {
+            grupoRepository.deleteById(grupoId);
+            grupoRepository.flush();
+        }catch (EmptyResultDataAccessException e){
+            throw new GrupoNaoEncontradoException(grupoId);
+        }catch (DataIntegrityViolationException e){
+            throw new EntidadeEmUsoException(
+                    String.format(MSG_GRUPO_EM_USO, grupoId));
+        }
+    }
+
+    @Transactional
+    public void desassociarGrupoService(Long grupoId, Long permissaoId){
+
+        Grupo grupo = buscarOuFalhar(grupoId);
+        Permissao permissao = cadastroPermissaoService.buscarOuFalhar(permissaoId);
+
+        grupo.removerPermissao(permissao);
+    }
+
+    @Transactional
+    public void associarPermissao(Long grupoId, Long permissaoId){
+
+        Grupo grupo = buscarOuFalhar(grupoId);
+        Permissao permissao = cadastroPermissaoService.buscarOuFalhar(permissaoId);
+
+        grupo.associarPermissao(permissao);
+    }
+
+    public Grupo buscarOuFalhar(Long grupoId){
+        return grupoRepository.findById(grupoId)
+                .orElseThrow(() -> new GrupoNaoEncontradoException(grupoId));
+    }
+
+
+
+}
---
+++ 12 - Modelagem avancada e implementacao da API/11 - endpoints de associacao de grupos com permissoes/algafood-api/src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
@@ -0,0 +1,87 @@
+package com.algaworks.algafood.domain.service;
+
+import com.algaworks.algafood.domain.model.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.algaworks.algafood.domain.exception.RestauranteNaoEncontradoException;
+import com.algaworks.algafood.domain.repository.RestauranteRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class CadastroRestauranteService {
+
+	@Autowired
+	private RestauranteRepository restauranteRepository;
+	
+	@Autowired
+	private CadastroCozinhaService cadastroCozinha;
+
+	@Autowired
+	private CadastroCidadeService cidadeService;
+
+	@Autowired
+	private CadastroFormaPagamentoService cadastroFormaPagamentoService;
+
+	@Transactional
+	public Restaurante salvar(Restaurante restaurante) {
+		Long cozinhaId = restaurante.getCozinha().getId();
+
+		Cozinha cozinha = cadastroCozinha.buscarOuFalhar(cozinhaId);
+		Cidade cidade =   cidadeService.buscarOuFalhar(restaurante.getEndereco().getCidade().getId());
+
+		restaurante.setCozinha(cozinha);
+		restaurante.getEndereco().setCidade(cidade);
+
+		return restauranteRepository.save(restaurante);
+	}
+
+	@Transactional
+	public void ativar (Long restauranteId){
+		Restaurante restaurante = buscarOuFalhar(restauranteId);
+
+		restaurante.ativar();
+	}
+
+	@Transactional
+	public void inativar (Long restauranteId){
+		Restaurante restaurante = buscarOuFalhar(restauranteId);
+
+		restaurante.inativar();
+	}
+
+	@Transactional
+	public void desassociarFormaPagamento(Long restauranteId, Long formapagamentoId){
+
+		Restaurante restaurante = buscarOuFalhar(restauranteId);
+		FormaPagamento formaPagamento = cadastroFormaPagamentoService.buscarOuFalhar(formapagamentoId);
+		restaurante.removerFormaPagamento(formaPagamento);
+	}
+
+	@Transactional
+	public void associarFormaPagamento(Long restauranteId, Long formapagamentoId){
+
+		Restaurante restaurante = buscarOuFalhar(restauranteId);
+		FormaPagamento formaPagamento = cadastroFormaPagamentoService.buscarOuFalhar(formapagamentoId);
+		restaurante.adicionarFormaPagamento(formaPagamento);
+	}
+
+	@Transactional
+	public void abrirRestaurante(Long restauranteId){
+		Restaurante restaurante = buscarOuFalhar(restauranteId);
+		restaurante.abrirRestaurante();
+	}
+
+	@Transactional
+	public void fecharRestaurante(Long restauranteId){
+		Restaurante restaurante = buscarOuFalhar(restauranteId);
+		restaurante.fecharRestaurante();
+	}
+	
+	public Restaurante buscarOuFalhar(Long restauranteId) {
+
+		return restauranteRepository.findById(restauranteId)
+			.orElseThrow(() -> new RestauranteNaoEncontradoException(restauranteId));
+	}
+	
+}
