83b19623014b5f0c0b00a7c42996d03f98bd3a75
rafaw <rafa.willian91@gmail.com>
2026-06-17 21:18:21 -0300

    Implementando serviço de cadastro de foto de produto

---
+++ 14 - Upload e download de arquivos/06 - Implementando servico de cadastro de foto de produto/algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoFotoController.java
@@ -0,0 +1,51 @@
+package com.algaworks.algafood.api.controller;
+
+import com.algaworks.algafood.api.assembler.FotoProdutoAssembler;
+import com.algaworks.algafood.api.model.dtos.FotoProdutoDTO;
+import com.algaworks.algafood.api.model.input.FotoProdutoInput;
+import com.algaworks.algafood.domain.model.FotoProduto;
+import com.algaworks.algafood.domain.model.Produto;
+import com.algaworks.algafood.domain.service.CadastroProdutoService;
+import com.algaworks.algafood.domain.service.CatalogoProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/restaurantes/{restauranteId}/produtos/{produtoId}/foto")
+public class RestauranteProdutoFotoController {
+
+    @Autowired
+    private CadastroProdutoService cadastroProdutoService;
+    @Autowired
+    private CatalogoProdutoService catalogoProdutoService;
+    @Autowired
+    private FotoProdutoAssembler fotoProdutoAssembler;
+    @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public FotoProdutoDTO atualizarFoto(@PathVariable Long restauranteId,
+                                        @PathVariable Long produtoId, @Valid FotoProdutoInput fotoProdutoInput) {
+
+        Produto produto = cadastroProdutoService.buscarOuFalhar(restauranteId, produtoId);
+
+        MultipartFile arquivo = fotoProdutoInput.getArquivo();
+
+        FotoProduto fotoProduto = new FotoProduto();
+        fotoProduto.setProduto(produto);
+        fotoProduto.setDescricao(fotoProdutoInput.getDescricao());
+        fotoProduto.setContentType(arquivo.getContentType());
+        fotoProduto.setTamanho(arquivo.getSize());
+        fotoProduto.setNomeArquivo(arquivo.getOriginalFilename());
+
+        FotoProduto fotoSalva = catalogoProdutoService.salvar(fotoProduto);
+        return fotoProdutoAssembler.toModel(fotoSalva);
+    }
+
+
+}
---
+++ 14 - Upload e download de arquivos/06 - Implementando servico de cadastro de foto de produto/algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/ProdutoRepositoryImpl.java
@@ -0,0 +1,20 @@
+package com.algaworks.algafood.infrastructure.repository;
+
+import com.algaworks.algafood.domain.model.FotoProduto;
+import com.algaworks.algafood.domain.repository.ProdutoRepositoryQueries;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+public class ProdutoRepositoryImpl implements ProdutoRepositoryQueries {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Transactional
+    @Override
+    public FotoProduto save(FotoProduto fotoProduto) {
+        return entityManager.merge(fotoProduto);
+    }
+}
