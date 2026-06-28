45a8a58540debc951057c0ea318770d4b0b742f8
rafaw <rafa.willian91@gmail.com>
2026-06-28 19:34:39 -0300

    Implementando recuperacao de foto no servico de armazenagem e consulta

---
+++ 14 - Upload e download de arquivos/11 - Implementando recuperacao de foto no servico de armazenagem e consulta/algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoFotoController.java
@@ -0,0 +1,58 @@
+package com.algaworks.algafood.api.controller;
+
+import com.algaworks.algafood.api.assembler.FotoProdutoAssembler;
+import com.algaworks.algafood.api.model.dtos.FotoProdutoDTO;
+import com.algaworks.algafood.api.model.input.FotoProdutoInput;
+import com.algaworks.algafood.domain.model.FotoProduto;
+import com.algaworks.algafood.domain.model.Produto;
+import com.algaworks.algafood.domain.service.CadastroProdutoService;
+import com.algaworks.algafood.domain.service.CatalogoProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/restaurantes/{restauranteId}/produtos/{produtoId}/foto")
+public class RestauranteProdutoFotoController {
+
+    @Autowired
+    private CadastroProdutoService cadastroProdutoService;
+    @Autowired
+    private CatalogoProdutoService catalogoProdutoService;
+    @Autowired
+    private FotoProdutoAssembler fotoProdutoAssembler;
+    @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public FotoProdutoDTO atualizarFoto(@PathVariable Long restauranteId,
+                                        @PathVariable Long produtoId, @Valid FotoProdutoInput fotoProdutoInput) throws IOException {
+
+        Produto produto = cadastroProdutoService.buscarOuFalhar(restauranteId, produtoId);
+
+        MultipartFile arquivo = fotoProdutoInput.getArquivo();
+
+        FotoProduto fotoProduto = new FotoProduto();
+        fotoProduto.setProduto(produto);
+        fotoProduto.setDescricao(fotoProdutoInput.getDescricao());
+        fotoProduto.setContentType(arquivo.getContentType());
+        fotoProduto.setTamanho(arquivo.getSize());
+        fotoProduto.setNomeArquivo(arquivo.getOriginalFilename());
+
+        FotoProduto fotoSalva = catalogoProdutoService.salvar(fotoProduto, arquivo.getInputStream());
+
+        return fotoProdutoAssembler.toModel(fotoSalva);
+    }
+
+    @GetMapping
+    public FotoProdutoDTO buscar(@PathVariable Long restauranteId, @PathVariable Long produtoId){
+
+        FotoProduto fotoProduto = catalogoProdutoService.buscarOuFalhar(restauranteId, produtoId);
+
+        return fotoProdutoAssembler.toModel(fotoProduto);
+    }
+}
