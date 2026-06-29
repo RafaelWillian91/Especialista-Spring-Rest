4f8d6c07c47ad34c1391a25c0f2b57346c11310a
rafaw <rafa.willian91@gmail.com>
2026-06-29 01:12:12 -0300

    Servindo arquivos de fotos pela API

---
+++ 14 - Upload e download de arquivos/12 - Servindo arquivos de fotos pela API/algafood-api/src/main/java/com/algaworks/algafood/domain/service/FluxoPedidoService.java
@@ -0,0 +1,38 @@
+package com.algaworks.algafood.domain.service;
+
+import com.algaworks.algafood.domain.exception.NegocioException;
+import com.algaworks.algafood.domain.model.Pedido;
+import com.algaworks.algafood.domain.model.StatusPedido;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.OffsetDateTime;
+
+@Service
+public class FluxoPedidoService {
+
+    @Autowired
+    private EmissaoPedidoService emissaoPedidoService;
+
+    @Transactional
+    public void confirmar(String codigoPedido) {
+       Pedido pedido = emissaoPedidoService.buscarOuFalhar(codigoPedido);
+       pedido.confirmar();
+
+    }
+
+    @Transactional
+    public void cancelarPedido(String codigoPedido) {
+        Pedido pedido = emissaoPedidoService.buscarOuFalhar(codigoPedido);
+        pedido.cancelar();
+    }
+
+    @Transactional
+    public void entregar(String codigoPedido) {
+        Pedido pedido = emissaoPedidoService.buscarOuFalhar(codigoPedido);
+        pedido.entregar();
+    }
+
+
+}
