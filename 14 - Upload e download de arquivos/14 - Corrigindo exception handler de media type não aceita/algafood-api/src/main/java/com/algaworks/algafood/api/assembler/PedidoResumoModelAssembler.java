9f108f8dfa6742450d9f8611f858da73663c98e7
rafaw <rafa.willian91@gmail.com>
2026-07-07 04:06:40 -0300

    Corrigindo exception handler de media type não aceita

---
+++ 14 - Upload e download de arquivos/14 - Corrigindo exception handler de media type não aceita/algafood-api/src/main/java/com/algaworks/algafood/api/assembler/PedidoResumoModelAssembler.java
@@ -0,0 +1,32 @@
+package com.algaworks.algafood.api.assembler;
+
+import com.algaworks.algafood.api.model.dtos.PedidoDTO;
+import com.algaworks.algafood.api.model.dtos.PedidoResumoDTO;
+import com.algaworks.algafood.domain.model.Pedido;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class PedidoResumoModelAssembler {
+
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public PedidoResumoDTO toModel (Pedido pedido){
+
+        return modelMapper.map(pedido, PedidoResumoDTO.class);
+    }
+
+    public List<PedidoResumoDTO> toCollectionModel(List<Pedido> pedidos) {
+        return pedidos.stream()
+                .map(pedido -> toModel(pedido))
+                .collect(Collectors.toList());
+    }
+
+
+}
