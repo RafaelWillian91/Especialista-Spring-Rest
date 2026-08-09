4c7fcb0becf76bf7e0d6c2faea64288bb9974f81
rafaw <rafa.willian91@gmail.com>
2026-08-09 02:28:32 -0300

    Observando e reagindo a Domain Events

---
+++ 15 - Emails transacionais e Domain Events/10 - Observando e reagindo a Domain Events/algafood-api/src/main/java/com/algaworks/algafood/domain/service/FotoStorageService.java
@@ -0,0 +1,52 @@
+package com.algaworks.algafood.domain.service;
+
+import com.algaworks.algafood.domain.model.FotoProduto;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.util.UUID;
+
+
+public interface FotoStorageService {
+    FotoRecuperada recuperar(String nomeArquivo);
+    void armazenar(NovaFoto novaFoto);
+    void remover (String nomeArquivo);
+    default void substituir(String nomeArquivoAntigo, NovaFoto novaFoto){
+        this.armazenar(novaFoto);
+
+        if(nomeArquivoAntigo != null){
+            this.remover(nomeArquivoAntigo);
+        }
+    }
+    default String gerarNomeArquivo (String nomeArquivo){
+        return UUID.randomUUID().toString() + "__" + nomeArquivo;
+    }
+    @Getter
+    @Builder
+    class NovaFoto {
+        private String nomeArquivo;
+        private String contentType;
+        private InputStream inputStream;
+
+    }
+
+    @Builder
+    @Getter
+    class FotoRecuperada{
+
+        private InputStream inputStream;
+        private String url;
+
+        public boolean temUrl(){
+            return url != null;
+        }
+
+        public boolean inputStream(){
+            return inputStream != null;
+        }
+    }
+}
