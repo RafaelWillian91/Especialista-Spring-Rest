b65c0bfdfdff15f77e48a1a21a2e6ca236756f0d
rafaw <rafa.willian91@gmail.com>
2026-08-14 01:00:37 -0300

    Reagindo a Domain Events em fases especificas da transacao

---
+++ 15 - Emails transacionais e Domain Events/11 - Reagindo a Domain Events em fases específicas da transacao/algafood-api/src/main/java/com/algaworks/algafood/domain/service/FluxoPedidoService.java
@@ -0,0 +1,42 @@
+package com.algaworks.algafood.domain.service;
+
+import com.algaworks.algafood.domain.exception.NegocioException;
+import com.algaworks.algafood.domain.model.Pedido;
+import com.algaworks.algafood.domain.model.StatusPedido;
+import com.algaworks.algafood.domain.repository.PedidoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.OffsetDateTime;
+
+@Service
+public class FluxoPedidoService {
+
+    @Autowired
+    private EmissaoPedidoService emissaoPedidoService;
+
+    @Autowired
+    private PedidoRepository pedidoRepository;
+    @Transactional
+    public void confirmar(String codigoPedido) {
+       Pedido pedido = emissaoPedidoService.buscarOuFalhar(codigoPedido);
+       pedido.confirmar();
+
+        pedidoRepository.save(pedido);
+    }
+
+    @Transactional
+    public void cancelarPedido(String codigoPedido) {
+        Pedido pedido = emissaoPedidoService.buscarOuFalhar(codigoPedido);
+        pedido.cancelar();
+    }
+
+    @Transactional
+    public void entregar(String codigoPedido) {
+        Pedido pedido = emissaoPedidoService.buscarOuFalhar(codigoPedido);
+        pedido.entregar();
+    }
+
+
+}
